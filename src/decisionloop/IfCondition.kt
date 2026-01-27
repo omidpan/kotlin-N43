5a77b263abe3b58dd82e5122ba47694e2aaa5cb9
leopn <omidpnteaching@gmail.com>
2026-01-27 09:01:02 -0500

    init kotlin

---
+++ src/decisionloop/IfCondition.kt
@@ -0,0 +1,31 @@
+package decisionloop
+
+fun main() {
+    val a = 2
+    val b = 3
+
+    var max = a
+    if (a < b) max = b
+
+    // With else
+    if (a > b) {
+        max = a
+    } else {
+        max = b
+    }
+
+    // As expression
+    max = if (a > b) a else b
+
+    // You can also use `else if` in expressions
+    val maxLimit = 1
+    val maxOrLimit =
+        if (maxLimit > a) maxLimit
+        else if (a > b) a
+        else b
+
+    println("max is $max")
+    println("maxOrLimit is $maxOrLimit")
+
+    println("++++++++++++++++++++++++ WHEN expression")
+}
