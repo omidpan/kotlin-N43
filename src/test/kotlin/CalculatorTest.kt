b8350151fd21b97ae6ed46e42ee8506c2e0b92e1
leopn <omidpnteaching@gmail.com>
2026-02-03 09:09:49 -0500

    exclude libs

---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,13 @@
+package test.kotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+
+    @Test
+    fun multiplyTest() {
+       assertEquals(4, 2 + 2)
+    }
+}
